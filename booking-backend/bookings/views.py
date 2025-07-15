aa5347ebe710941ae9a9d6b738a577979697af30
Damjan Pavlovic <pavlovic.damjan1828@gmail.com>
2025-07-15 15:36:39 +0200

    Added functioning delete button for reservations

--- booking-backend/bookings/views.py
+++ booking-backend/bookings/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Booking
 from .serializers import BookingSerializer
 # Create your views here.
 
 class BookingListCreateView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
+class BookingRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
--- booking-frontend/src/BookingList.jsx
+++ booking-frontend/src/BookingList.jsx
@@ -13,6 +13,19 @@ const BookingList = forwardRef((props, ref) => {
       .catch((err) => console.error("Greška:", err));
   };
 
+  const deleteBooking = async (id) => {
+    const confirmDelete = window.confirm("Da li sigurno zelis da obrises ovu rezervaciju?")
+    if (!confirmDelete) return;
+
+    try {
+      await axios.delete(`http://localhost:8000/api/bookings/${id}/`);
+      fetchBookings();
+    } catch (err) {
+      console.error("Greska pri brisanju:", err);
+      alert("Greska pri brisanju. Pokusaj ponovo.")
+    }
+  };
+
   useImperativeHandle(ref, () => ({
     refresh: fetchBookings,
   }));
@@ -34,12 +47,23 @@ const BookingList = forwardRef((props, ref) => {
               key={booking.id}
               className="border border-gray-200 dark:border-gray-700 p-4 rounded-lg shadow-sm bg-gray-50 dark:bg-gray-700"
             >
-              <p className="text-lg font-semibold">{booking.full_name}</p>
-              <p className="text-sm text-gray-500 dark:text-gray-300">{booking.email}</p>
-              <p className="text-sm text-gray-700 dark:text-gray-200 mt-2">
-                <strong>Od:</strong> {booking.date_from} <br />
-                <strong>Do:</strong> {booking.date_to}
-              </p>
+              <div className="flex justify-between items-start">
+                <div>
+                  <p className="text-lg font-semibold">{booking.full_name}</p>
+                  <p className="text-sm text-gray-500 dark:text-gray-300">{booking.email}</p>
+                  <p className="text-sm text-gray-700 dark:text-gray-200 mt-2">
+                    <strong>Od:</strong> {booking.date_from} <br />
+                    <strong>Do:</strong> {booking.date_to}
+                  </p>
+                </div>
+
+                <button
+                  onClick={() => deleteBooking(booking.id)}
+                  className="ml-4 bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded-lg text-sm transition"
+                >
+                  Obriši
+                </button>
+              </div>
             </li>
           ))}
         </ul>
